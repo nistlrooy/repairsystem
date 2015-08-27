03388bd57fdb06086791624bccbc73c78ecc7615
nistlrooy <fjlyzsc@gmail.com>
2015-08-27 22:24:18 +0800

    update

--- src/App/RepairBundle/Admin/FaultInfoAdmin.php
+++ src/App/RepairBundle/Admin/FaultInfoAdmin.php
@@ -8,7 +8,7 @@ use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Form\FormMapper;
 
-class RepairInfoAdmin extends Admin
+class FaultInfoAdmin extends Admin
 {
     // Fields to be shown on create/edit forms
     protected function configureFormFields(FormMapper $formMapper)
