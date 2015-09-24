c7a1cf0b7274f0e86bbdd162ba08ced18ad20878
nistlrooy <fjlyzsc@gmail.com>
2015-09-24 10:50:18 +0800

    update
    添加liuggio/exce Bundle，准备实现生成excel功能

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -35,6 +35,7 @@ class AppKernel extends Kernel
             // Then add SonataAdminBundle
             new Sonata\AdminBundle\SonataAdminBundle(),
             new FOS\RestBundle\FOSRestBundle(),
+            new Liuggio\ExcelBundle\LiuggioExcelBundle(),
 
 
             new App\UserBundle\UserBundle(),
